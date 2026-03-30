96418ecde13d0971bc552f2f9b2351cebf141405
YonyUk <yonatanjoseguerraperez@gmail.com>
2026-03-30 17:07:42 -0400

    adds aux constructor for InvalidUserRoleException

--- Services/Users/Domain/Exceptions/InvalidUserRoleException.cs
+++ Services/Users/Domain/Exceptions/InvalidUserRoleException.cs
@@ -4,8 +4,14 @@ namespace Users.Domain.Exceptions;
 
 public class InvalidUserRoleException : InvalidValueException
 {
-    public InvalidUserRoleException(string role)
-    : base($"{role} is not a valid value for field Role")
+    public InvalidUserRoleException(string role,string[] validNames)
+    : base($"Value {role} is not a valid value for field Role. Valid values: {string.Join(", ",validNames)}")
+    {
+        
+    }
+
+    public InvalidUserRoleException(int role, int[] validValues)
+    : base($"Value {role} is not a numeric value valid for field Role. Valid values: {string.Join(", ",validValues)}")
     {
         
     }
