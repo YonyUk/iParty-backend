10da850b2b76b9faa5240630f77ec0d3d323fc83
YonyUk <yonatanjoseguerraperez@gmail.com>
2026-04-01 13:25:38 -0400

    adds argument null checking to UserTests

--- Tests/Users/Domain/Aggregates/UserTests.cs
+++ Tests/Users/Domain/Aggregates/UserTests.cs
@@ -10,17 +10,23 @@ namespace Tests.Unit.Users.Domain.Aggregates;
 public class UserTests
 {
 <<<<<<< HEAD
+<<<<<<< HEAD
     private readonly UserNameDomainRules rules = new UserNameDomainRules(6, 10);
 
 =======
     private readonly UserNameDomainRules rules = new UserNameDomainRules(6,10);
     
 >>>>>>> f4f0ea9 (adds UserTests)
+=======
+    private readonly UserNameDomainRules rules = new UserNameDomainRules(6, 10);
+
+>>>>>>> f74ea6d (adds argument null checking to UserTests)
     [Theory]
     [InlineData(UserRole.User)]
     [InlineData(UserRole.Host)]
     public void TestCreateUser(UserRole role)
     {
+<<<<<<< HEAD
 <<<<<<< HEAD
         var username = new UserName("yonyuk", rules);
         var email = new Email("user@gmail.com");
@@ -36,6 +42,14 @@ public class UserTests
         var user = new User(username,email,hashedPassword,role);
         
 >>>>>>> f4f0ea9 (adds UserTests)
+=======
+        var username = new UserName("yonyuk", rules);
+        var email = new Email("user@gmail.com");
+        var hashedPassword = new HashedPassword("asdgeweiusbj");
+
+        var user = new User(username, email, hashedPassword, role);
+
+>>>>>>> f74ea6d (adds argument null checking to UserTests)
         user.Id.Should().NotBe(Guid.Empty);
         user.UserName.Should().Be(username);
         user.Email.Should().Be(email);
@@ -45,26 +59,35 @@ public class UserTests
         user.Events.Should().ContainSingle()
             .Which.Should().BeOfType<UserRegisteredEvent>()
 <<<<<<< HEAD
+<<<<<<< HEAD
             .And.Match<UserRegisteredEvent>(e =>
 =======
             .And.Match<UserRegisteredEvent>(e => 
 >>>>>>> f4f0ea9 (adds UserTests)
+=======
+            .And.Match<UserRegisteredEvent>(e =>
+>>>>>>> f74ea6d (adds argument null checking to UserTests)
                 e.Id == user.Id &&
                 e.UserName == user.UserName &&
                 e.Email == user.Email &&
                 e.Role == user.Role);
     }
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 =======
     
 >>>>>>> f4f0ea9 (adds UserTests)
+=======
+
+>>>>>>> f74ea6d (adds argument null checking to UserTests)
     [Theory]
     [InlineData(UserRole.User)]
     [InlineData(UserRole.Host)]
     public void TestInternalCreateUser(UserRole role)
     {
         var id = Guid.NewGuid();
+<<<<<<< HEAD
 <<<<<<< HEAD
         var username = new UserName("yonyuk", rules);
         var email = new Email("user@gmail.com");
@@ -80,6 +103,14 @@ public class UserTests
         var user = new User(id,username,email,hashedPassword,role);
         
 >>>>>>> f4f0ea9 (adds UserTests)
+=======
+        var username = new UserName("yonyuk", rules);
+        var email = new Email("user@gmail.com");
+        var hashedPassword = new HashedPassword("asdgeweiusbj");
+
+        var user = new User(id, username, email, hashedPassword, role);
+
+>>>>>>> f74ea6d (adds argument null checking to UserTests)
         user.Id.Should().Be(id);
         user.UserName.Should().Be(username);
         user.Email.Should().Be(email);
@@ -87,6 +118,9 @@ public class UserTests
         user.Role.Should().Be(role);
     }
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> f74ea6d (adds argument null checking to UserTests)
 
     [Fact]
     public void TestChangeUserPassword()
@@ -127,6 +161,9 @@ public class UserTests
         action2.Should().Throw<ArgumentNullException>();
         action3.Should().Throw<ArgumentNullException>();
     }
+<<<<<<< HEAD
 =======
 >>>>>>> f4f0ea9 (adds UserTests)
+=======
+>>>>>>> f74ea6d (adds argument null checking to UserTests)
 }
