bb66fca336b7f827de7bc3a83586139da78e098f
YonyUk <yonatanjoseguerraperez@gmail.com>
2026-04-01 12:08:35 -0400

    adds User creation test

---
+++ Tests/Users/Domain/ValueObjects/UserNameTests.cs
@@ -0,0 +1,28 @@
+using FluentAssertions;
+using Users.Domain.Exceptions;
+using Users.Domain.Rules;
+using Users.Domain.ValueObjects;
+
+namespace Tests.Unit.Users.Domain.ValueObjects;
+
+public class UserNameTests
+{
+    private readonly UserNameDomainRules rules = new UserNameDomainRules(6,10);
+
+    [Theory]
+    [InlineData("yonyuk",true)]
+    [InlineData("yony",false)]
+    public void TestCreateUser(string username,bool valid)
+    {
+        var action = () =>
+        {
+            var usernameObject = new UserName(username,rules);
+            if (valid)
+                usernameObject.Value.Should().Be(username);
+        };
+        if (valid)
+            action.Should().NotThrow();
+        else
+            action.Should().Throw<InvalidUserNameException>();
+    }
+}
