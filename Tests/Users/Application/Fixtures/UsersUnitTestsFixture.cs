85008cd70aa76bc817cb5acc8a7f04ba79e31bc9
YonyUk <yonatanjoseguerraperez@gmail.com>
2026-04-04 00:05:21 -0400

    adds GetUsersByRoleQueryHandlerTests and move Users population and IMapper mock to UsersUnitTestFixture

---
+++ Tests/Users/Application/Fixtures/UsersUnitTestsFixture.cs
@@ -0,0 +1,53 @@
+
+using AutoMapper;
+using NSubstitute;
+using Users.Application.DTOs;
+using Users.Domain;
+using Users.Domain.Aggregates;
+using Users.Domain.Rules;
+using Users.Domain.ValueObjects;
+
+namespace Tests.Unit.Users.Fixtures;
+
+public class UsersUnitTestsFixture : IAsyncLifetime
+{
+    public IEnumerable<User> Users { get; private set; }
+    public IMapper MockedMapper { get; private set; }
+    private readonly UserNameDomainRules rules = new UserNameDomainRules(6, 10);
+    public UsersUnitTestsFixture()
+    {
+        Users = Populate("yonyuk", "jose01", "brayan", "cuervo", "nayeli");
+        MockedMapper = Substitute.For<IMapper>();
+        MockedMapper.Map<IEnumerable<UserDTO>>(Arg.Any<IEnumerable<User>>())
+            .Returns(callInfo => callInfo.Arg<IEnumerable<User>>()
+                    .Select(user => new UserDTO(
+                        user.Id,
+                        user.UserName.Value,
+                        user.Email.Value,
+                        user.Role
+                    )
+                )
+            );
+    }
+    public Task DisposeAsync() => Task.CompletedTask;
+
+    public async Task InitializeAsync()
+    {
+        // Users = Populate("yonyuk","jose01","brayan","cuervo","nayeli");
+    }
+    IEnumerable<User> Populate(params string[] names)
+    {
+        var role = 0;
+        foreach (var name in names)
+        {
+            var username = new UserName(name, rules);
+            var email = new Email($"{name}@gmail.com");
+            var hashedPassword = new HashedPassword($"{name}_password_hashed");
+            yield return new User(username, email, hashedPassword,(UserRole)role);
+            role ^= 1;
+        }
+    }
+}
+
+[CollectionDefinition("Users Collection Fixture For Unit Testing On Application Layer")]
+public class UsersUnitTestsFixtureShared : ICollectionFixture<UsersUnitTestsFixture> { }
---
+++ Tests/Users/Application/Queries/GetUsersByRoleQueryHandlerTests.cs
@@ -0,0 +1,47 @@
+using AutoMapper;
+using FluentAssertions;
+using NSubstitute;
+using Tests.Unit.Users.Fixtures;
+using Users.Application.DTOs;
+using Users.Application.Querys;
+using Users.Application.Querys.Handlers;
+using Users.Domain;
+using Users.Domain.Aggregates;
+using Users.Domain.Rules;
+using Users.Domain.ValueObjects;
+
+namespace Tests.Unit.Users.Application.Queries.Handlers;
+
+[Collection("Users Collection Fixture For Unit Testing On Application Layer")]
+public class GetUsersByRoleQueryHandlerTests
+{
+    private readonly IUserRepository userRepository;
+    private readonly IMapper mapper;
+    private readonly IEnumerable<User> users;
+    public GetUsersByRoleQueryHandlerTests(UsersUnitTestsFixture fixture)
+    {
+        userRepository = Substitute.For<IUserRepository>();
+        mapper = fixture.MockedMapper;
+        users = fixture.Users;
+    }
+
+    [Theory]
+    [InlineData(UserRole.User)]
+    [InlineData(UserRole.Host)]
+    public async Task TestGetUsersByRole(UserRole role)
+    {
+        var expectedUsers = users.Where(user => user.Role == role);
+        userRepository.GetUsersByRole(role).Returns(expectedUsers);
+
+        var query = new GetUsersByRoleQuery(role);
+        var handler = new GetUsersByRoleQueryHandler(userRepository,mapper);
+
+        var usersResult = await handler.Handle(query,CancellationToken.None);
+        
+        await userRepository.Received(1).GetUsersByRole(role,Arg.Any<CancellationToken>());
+        mapper.Received(1).Map<IEnumerable<UserDTO>>(expectedUsers);
+        
+        usersResult.Should().OnlyContain(userDTO => userDTO.Role == role);
+        usersResult.Should().HaveCount(expectedUsers.Count());
+    }
+}
