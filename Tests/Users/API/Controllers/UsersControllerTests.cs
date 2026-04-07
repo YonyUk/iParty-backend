f6af84bcd06d149aa1115859bbff79df2f468138
YonyUk <yonatanjoseguerraperez@gmail.com>
2026-04-07 07:59:55 -0400

    adds TestLogin test to UserControllerTests

--- Tests/Users/API/Controllers/UsersControllerTests.cs
+++ Tests/Users/API/Controllers/UsersControllerTests.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Mvc;
 using FluentAssertions;
 using MediatR;
 using Microsoft.Extensions.Options;
@@ -9,8 +10,16 @@ using Users.Application.DTOs;
 using Users.Domain;
 using Users.Domain.Exceptions;
 using Users.Infrastructure.Configuration;
+using Microsoft.AspNetCore.Http;
 
 namespace Tests.Unit.Users.API.Controllers;
+
+public enum LoginUserResult
+{
+    UserNotFound = 0,
+    WrongUsernameOrPassword = 1,
+    Success = 2
+}
 
 public class UsersControllerTests
 {
@@ -24,28 +33,76 @@ public class UsersControllerTests
         {
             ExpiresMinutes = 30
         });
-        controller = new UsersController(mediator,jwtConfigOptions);
+        controller = new UsersController(mediator, jwtConfigOptions);
     }
     [Theory]
-    [InlineData("yonyuk","user@gmail.com","yony01uk",UserRole.User,true)]
-    [InlineData("yonyuk","user@gmail.com","yony01uk",UserRole.User,false)]
-    public async Task TestCreateUser(string username,string email,string password,UserRole role,bool success)
+    [InlineData(true)]
+    [InlineData(false)]
+    public async Task TestCreateUser(bool expected)
     {
-        if (!success)
-            mediator.Send(Arg.Any<RegisterUserCommand>(),Arg.Any<CancellationToken>())
-                .Throws(new UserAlreadyExistsException("username",username));
+        var username = "yonyuk";
+        var email = "user@gmail.com";
+        var password = "yony01uk";
+        var role = UserRole.User;
+
         var action = async () =>
-        {  
-            var data = new RegisterUserDTO(username,email,password,role);
+        {
+            var data = new RegisterUserDTO(username, email, password, role);
             var command = new RegisterUserCommand(data);
             return await controller.CreateUser(command);
         };
-        if (success)
+    }
+
+    [Theory]
+    [InlineData(LoginUserResult.Success)]
+    [InlineData(LoginUserResult.WrongUsernameOrPassword)]
+    [InlineData(LoginUserResult.UserNotFound)]
+    public async Task TestLogin(LoginUserResult expected)
+    {
+        var username = "yonyuk";
+        var password = "yony01uk";
+
+        var data = new LoginUserDTO(username, password);
+
+        switch (expected)
         {
-            await action.Should().NotThrowAsync();
-            await mediator.Received(1).Send(Arg.Any<RegisterUserCommand>(),Arg.Any<CancellationToken>());
+            case LoginUserResult.UserNotFound:
+                mediator.Send(Arg.Any<LoginUserCommand>(),Arg.Any<CancellationToken>())
+                    .ThrowsAsync(new UserNotFoundException("username",username));
+                break;
+
+            case LoginUserResult.WrongUsernameOrPassword:
+                mediator.Send(Arg.Any<LoginUserCommand>(), Arg.Any<CancellationToken>())
+                    .Returns(new LoginResponseDTO(null, "Incorrect username or password"));
+                break;
+
+            default:
+                mediator.Send(Arg.Any<LoginUserCommand>(), Arg.Any<CancellationToken>())
+                    .Returns(new LoginResponseDTO("token", null));
+                break;
         }
-        else
-            await action.Should().ThrowAsync<UserAlreadyExistsException>();
+
+        var context = new DefaultHttpContext();
+        controller.ControllerContext = new ControllerContext { HttpContext = context };
+
+        var action = async () => await controller.Login(data);
+
+        switch (expected)
+        {
+            case LoginUserResult.UserNotFound:
+                await action.Should().ThrowAsync<UserNotFoundException>();
+                break;
+            case LoginUserResult.WrongUsernameOrPassword:
+                var result1 = await action();
+                result1.Should().BeOfType<UnauthorizedObjectResult>();
+                break;
+
+            default:
+                var result2 = await action();
+                await mediator.Received(1).Send(Arg.Any<LoginUserCommand>(), Arg.Any<CancellationToken>());
+                result2.Should().BeOfType<AcceptedResult>();
+                context.Response.Headers["Set-Cookie"].ToString().Should().Contain("access_token=token");
+                break;
+        }
     }
 }
