94aef703474d6915cc4286cdec51379b0b629ee0
YonyUk <yonatanjoseguerraperez@gmail.com>
2026-04-07 08:18:57 -0400

    adds TestGetUserById test to UsersControllerTests

--- Tests/Users/API/Controllers/UsersControllerTests.cs
+++ Tests/Users/API/Controllers/UsersControllerTests.cs
@@ -11,6 +11,10 @@ using Users.Domain;
 using Users.Domain.Exceptions;
 using Users.Infrastructure.Configuration;
 using Microsoft.AspNetCore.Http;
+using Users.Domain.Rules;
+using Users.Domain.ValueObjects;
+using Users.Domain.Aggregates;
+using Users.Application.Querys;
 
 namespace Tests.Unit.Users.API.Controllers;
 
@@ -23,6 +27,7 @@ public enum LoginUserResult
 
 public class UsersControllerTests
 {
+    private readonly UserNameDomainRules rules = new UserNameDomainRules(6, 10);
     private readonly IMediator mediator;
     private readonly IOptions<JwtConfigOptions> jwtConfigOptions;
     private readonly UsersController controller;
@@ -67,8 +72,8 @@ public class UsersControllerTests
         switch (expected)
         {
             case LoginUserResult.UserNotFound:
-                mediator.Send(Arg.Any<LoginUserCommand>(),Arg.Any<CancellationToken>())
-                    .ThrowsAsync(new UserNotFoundException("username",username));
+                mediator.Send(Arg.Any<LoginUserCommand>(), Arg.Any<CancellationToken>())
+                    .ThrowsAsync(new UserNotFoundException("username", username));
                 break;
 
             case LoginUserResult.WrongUsernameOrPassword:
@@ -104,5 +109,37 @@ public class UsersControllerTests
                 context.Response.Headers["Set-Cookie"].ToString().Should().Contain("access_token=token");
                 break;
         }
+    }
+
+    [Theory]
+    [InlineData(true)]
+    [InlineData(false)]
+    public async Task TestGetUserById(bool exists)
+    {
+
+        if (exists)
+        {
+            var username = new UserName("yonyuk", rules);
+            var email = new Email("user@gmail.com");
+            var hash = new HashedPassword("hash");
+            var user = new User(username, email, hash);
+
+            mediator.Send(Arg.Any<GetUserByIdQuery>(),Arg.Any<CancellationToken>())
+                .Returns(new UserDTO(user.Id,user.UserName.Value,user.Email.Value,user.Role));
+        }
+        else
+            mediator.Send(Arg.Any<GetUserByIdQuery>(), Arg.Any<CancellationToken>())
+                .ThrowsAsync(new UserNotFoundException("id", "id"));
+        
+        var action = async () => await controller.GetUserById(Guid.NewGuid());
+
+        if (exists)
+        {
+            var result = await action();
+            result.Should().BeOfType<OkObjectResult>();
+            await mediator.Received(1).Send(Arg.Any<GetUserByIdQuery>(),Arg.Any<CancellationToken>());
+        }
+        else
+            await action.Should().ThrowAsync<UserNotFoundException>();
     }
 }
