742a5324857510bbbedff309a6df866ebf8dbf7a
YonyUk <yonatanjoseguerraperez@gmail.com>
2026-04-08 03:35:12 -0400

    adds CustomWebApplicationFactory

---
+++ Tests/Users/Integration/CustomWebApplicationFactory.cs
@@ -0,0 +1,50 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using Testcontainers.PostgreSql;
+using Users.Infrastructure.Persistence;
+
+namespace Tests.Integration.Users;
+
+public class CustomWebApplicationFactory : WebApplicationFactory<Program>, IAsyncLifetime
+{
+    private readonly PostgreSqlContainer dbContainer;
+    public CustomWebApplicationFactory()
+    {
+        dbContainer = new PostgreSqlBuilder("postgres:16-alphine")
+            .WithDatabase("testdb")
+            .WithUsername("testuser")
+            .WithPassword("testpassword")
+            .WithCleanUp(true)
+            .Build();
+    }
+    public async Task InitializeAsync()
+    {
+        await dbContainer.StartAsync();
+    }
+
+    async Task IAsyncLifetime.DisposeAsync()
+    {
+        await dbContainer.DisposeAsync();
+    }
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureTestServices(services =>
+        {
+            services.RemoveAll(typeof(DbContextOptions<AppDbContext>));
+
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseNpgsql(dbContainer.GetConnectionString());
+            });
+
+            using var scope = services.BuildServiceProvider().CreateScope();
+            var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+            dbContext.Database.Migrate();
+        });
+    }
+}
