b0f4ba5670de62c6a93fcae6cb32874b4240f0cd
YonyUk <yonatanjoseguerraperez@gmail.com>
2026-04-09 01:51:49 -0400

    adds TestCreateUser test

--- Tests/Users/Integration/UsersApiIntegrationTests.cs
+++ Tests/Users/Integration/UsersApiIntegrationTests.cs
@@ -6,19 +6,53 @@ using Users.Domain;
 
 namespace Tests.Integration.Users;
 
+public enum TestCreateUserExpectedResult
+{
+    Ok = 0,
+    BadRequest,
+    Conflict
+}
 public class UsersApiIntegrationTests : UsersBaseIntegrationTests
 {
     public UsersApiIntegrationTests(CustomWebApplicationFactory applicationFactory) : base(applicationFactory)
     {
-        
+
     }
 
-    [Fact]
-    public async Task TestCreateUser()
+    [Theory]
+    [InlineData("yonyuk", "user@gmail.com", "yony01uk", UserRole.User, TestCreateUserExpectedResult.Ok)]
+    [InlineData("yonyuk", "user@gmail.com", "yony01uk", UserRole.Host, TestCreateUserExpectedResult.Ok)]
+    [InlineData("yony", "user@gmail.com", "yony01uk", UserRole.User, TestCreateUserExpectedResult.BadRequest)]
+    [InlineData("yonyuk", "usergmail.com", "yony01uk", UserRole.User, TestCreateUserExpectedResult.BadRequest)]
+    [InlineData("yonyuk", "user@gmail.com", "yony", UserRole.User, TestCreateUserExpectedResult.BadRequest)]
+    [InlineData(null, "user@gmail.com", "yony01uk", UserRole.User, TestCreateUserExpectedResult.BadRequest)]
+    [InlineData("yonyuk", null, "yony01uk", UserRole.User, TestCreateUserExpectedResult.BadRequest)]
+    [InlineData("yonyuk", "user@gmail.com", null, UserRole.User, TestCreateUserExpectedResult.BadRequest)]
+    [InlineData("   ", "user@gmail.com", "yony01uk", UserRole.User, TestCreateUserExpectedResult.BadRequest)]
+    [InlineData("yonyuk", "   ", "yony01uk", UserRole.User, TestCreateUserExpectedResult.BadRequest)]
+    [InlineData("yonyuk", "user@gmail.com", "   ", UserRole.User, TestCreateUserExpectedResult.BadRequest)]
+    [InlineData("yonyuk", "user@gmail.com", "yony01uk", UserRole.User, TestCreateUserExpectedResult.Conflict)]
+    public async Task TestCreateUser(string? username, string? email, string? password, UserRole role, TestCreateUserExpectedResult expected)
     {
-        var data = new RegisterUserDTO("yonyuk","user@gmail.com","yony01uk",UserRole.User);
+        var data = new RegisterUserDTO(username, email, password, role);
+        var response = await client.PostAsJsonAsync("/api/users/register", data);
 
-        var response = await client.PostAsJsonAsync("/api/users/register",data);
-        response.StatusCode.Should().Be(HttpStatusCode.Created);
+        if (expected == TestCreateUserExpectedResult.Conflict)
+            response = await client.PostAsJsonAsync("api/users/register",data);
+
+        switch (expected)
+        {
+            case TestCreateUserExpectedResult.BadRequest:
+                response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+                break;
+
+            case TestCreateUserExpectedResult.Conflict:
+                response.StatusCode.Should().Be(HttpStatusCode.Conflict);
+                break;
+                
+            default:
+                response.StatusCode.Should().Be(HttpStatusCode.Created);
+                break;
+        }
     }
 }
