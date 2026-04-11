d0399a8e44e913e48f71737ba01211f2269d3e22
YonyUk <yonatanjoseguerraperez@gmail.com>
2026-04-11 05:23:35 -0400

    creates the gateaway

---
+++ API_Gateaway/Program.cs
@@ -0,0 +1,32 @@
+using ApiGateaway.Configuration;
+using ApiGateaway.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.Configure<RateLimitOptions>(
+    opt => builder.Configuration.GetSection("RateLimitOptions").Bind(opt)
+);
+
+builder.Services.AddAuthenticationService(builder.Configuration);
+builder.Services.AddAuthorizationService();
+builder.Services.AddHealthChecks();
+builder.Services.AddRateLimitService();
+builder.Services.AddReverseProxyService(builder.Configuration);
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
+app.UseRateLimiter();
+app.MapHealthChecks("/health");
+
+app.MapGet("/ingo", () => Results.Ok(new
+{
+    GateAway = "Yarp",
+    Status = "Running",
+    Timestamp = DateTime.UtcNow
+}));
+
+app.MapReverseProxy();
+app.Run();
