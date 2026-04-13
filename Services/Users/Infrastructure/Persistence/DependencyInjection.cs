5bca09a1d2b7955972d0e759b8d342751b93c7ab
YonyUk <yonatanjoseguerraperez@gmail.com>
2026-04-13 05:50:01 -0400

    adds dual format support for Npgsql connection string

--- Services/Users/Infrastructure/Persistence/DependencyInjection.cs
+++ Services/Users/Infrastructure/Persistence/DependencyInjection.cs
@@ -15,6 +15,7 @@ using Users.Infrastructure.Persistence.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
+using Npgsql;
 
 namespace Users.Infrastructure.DependencyInjection;
 
@@ -41,7 +42,9 @@ public static class DependencyInjection
     {
         services.AddDbContext<AppDbContext>((sp,options) =>
         {
-            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            var rawConnectionString = configuration.GetConnectionString("DefaultConnection");
+            var csBuilder = new NpgsqlConnectionStringBuilder(rawConnectionString);
+            var connectionString = csBuilder.ConnectionString;
             var dbOptions = sp.GetRequiredService<IOptions<DatabaseConfigOptions>>().Value;
 
             options.UseNpgsql(connectionString,npgsqlOptions =>
