84445b5d957109c0b30ccedbc9eafdb7fed8e0f4
YonyUk <yonatanjoseguerraperez@gmail.com>
2026-04-13 06:56:00 -0400

    adds connectionStringExplicit parsing

--- Services/Users/Infrastructure/Persistence/DependencyInjection.cs
+++ Services/Users/Infrastructure/Persistence/DependencyInjection.cs
@@ -21,43 +21,43 @@ namespace Users.Infrastructure.DependencyInjection;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddPersistenceService(this IServiceCollection services,IConfiguration configuration)
+    public static IServiceCollection AddPersistenceService(this IServiceCollection services, IConfiguration configuration)
     {
         var securitySection = configuration.GetSection("Security");
         services.Configure<UserDomainRulesOptions>(opt => configuration.GetSection("UsersDomain").Bind(opt));
         services.Configure<DatabaseConfigOptions>(opt => configuration.GetSection("Database").Bind(opt));
         services.Configure<JwtConfigOptions>(opt => securitySection.GetSection("Jwt").Bind(opt));
-        services.AddScoped<IPasswordHasher,PasswordHasher>();
-        services.AddScoped<IUserDomainRulesConfigProvider,UserDomainRulesConfigProvider>();
-        services.AddScoped<IUserAuthenticator,UserAuthenticator>();
+        services.AddScoped<IPasswordHasher, PasswordHasher>();
+        services.AddScoped<IUserDomainRulesConfigProvider, UserDomainRulesConfigProvider>();
+        services.AddScoped<IUserAuthenticator, UserAuthenticator>();
         services.InjectDatabaseService(configuration);
         services.InjectAuthenticationService(configuration);
-        services.AddScoped<IUserRepository,UserRepository>();
-        services.AddScoped<IUnitOfWork,UnitOfWork>();
-        services.AddScoped<IUserUniquenessChecker,UserUniquenessChecker>();
+        services.AddScoped<IUserRepository, UserRepository>();
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+        services.AddScoped<IUserUniquenessChecker, UserUniquenessChecker>();
         return services;
     }
 
-    static IServiceCollection InjectDatabaseService(this IServiceCollection services,IConfiguration configuration)
+    static IServiceCollection InjectDatabaseService(this IServiceCollection services, IConfiguration configuration)
     {
-        services.AddDbContext<AppDbContext>((sp,options) =>
+        services.AddDbContext<AppDbContext>((sp, options) =>
         {
             var rawConnectionString = configuration.GetConnectionString("DefaultConnection");
             if (string.IsNullOrEmpty(rawConnectionString) || string.IsNullOrWhiteSpace(rawConnectionString))
                 rawConnectionString = Environment.GetEnvironmentVariable("ConnectionStrings__DefaultConnection");
-            System.Console.WriteLine($"Connection string: {rawConnectionString}");
+            rawConnectionString = ParseConnectionStringValue(rawConnectionString!);
             var csBuilder = new NpgsqlConnectionStringBuilder(rawConnectionString);
             var connectionString = csBuilder.ConnectionString;
             var dbOptions = sp.GetRequiredService<IOptions<DatabaseConfigOptions>>().Value;
 
-            options.UseNpgsql(connectionString,npgsqlOptions =>
+            options.UseNpgsql(connectionString, npgsqlOptions =>
             {
                 npgsqlOptions.CommandTimeout(dbOptions.CommandTimeoutSeconds);
             });
 
             if (dbOptions.EnableSensitiveDataLogging)
                 options.EnableSensitiveDataLogging();
-            
+
         });
         return services;
     }
@@ -78,7 +78,8 @@ public static class DependencyInjection
                     IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtConfigSection.GetValue<string>("SecretKey") ?? ""))
                 };
 
-                options.Events = new JwtBearerEvents{
+                options.Events = new JwtBearerEvents
+                {
                     OnMessageReceived = context =>
                     {
                         context.Token = context.Request.Cookies["access_token"];
@@ -87,5 +88,24 @@ public static class DependencyInjection
                 };
             });
         return services;
+    }
+    static string ParseConnectionStringValue(string connectionString)
+    {
+        if (connectionString.Contains("Host=") && connectionString.Contains("Database="))
+            return connectionString;
+
+        if (!connectionString.StartsWith("postgres://", StringComparison.OrdinalIgnoreCase) &&
+            !connectionString.StartsWith("postgresql://", StringComparison.OrdinalIgnoreCase))
+            return connectionString;
+
+        var uri = new Uri(connectionString);
+        var userInfo = uri.UserInfo.Split(':');
+        var username = userInfo[0];
+        var password = userInfo[1];
+        var database = uri.AbsolutePath.TrimStart('/');
+        var host = uri.Host;
+        var port = uri.Port > 0 ? uri.Port : 5432;
+
+        return $"Host={host};Port={port};Database={database};Username={username};Password={password};SSL Mode=Require;Trust Server Certificate=true;";
     }
 }
